3764732eead51a56c14624ae490d0fc637a48b76
mmoremol <mariano.moreno@orange.com>
2019-08-19 13:52:02 +0200

    - Added Input variable to component for the example.

--- src/app/shortenedname/shortenedname.component.ts
+++ src/app/shortenedname/shortenedname.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   // selector: 'app-shortenedname',
@@ -6,6 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./shortenedname.component.css']
 })
 export class ShortenednameComponent implements OnInit {
+  @Input() public name: string;
 
   constructor() {
   }
