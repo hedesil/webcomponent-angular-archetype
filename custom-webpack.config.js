7f1f030d7c011806ea583195441d6133b26b0429
mmoremol <mariano.moreno@orange.com>
2019-08-19 10:48:03 +0200

    Deleted all files instead of a clean Angular 8 project.

--- custom-webpack.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const customPackage = require(path.join(__dirname, '.', 'package.json'));
-
-module.exports = {
-    output: {
-      jsonpFunction: `webpackJsonp-${customPackage.name}`
-    }
-};
